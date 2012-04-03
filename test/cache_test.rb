c18153ee880229fe00c6d5d20d3c2ca4a817ddf6
radiospiel <eno@open-lab.org>
2012-04-03 13:16:33 +0200

    Cleans up cache test, updates textmate's project file exclusion pattern

--- test/cache_test.rb
+++ test/cache_test.rb
@@ -2,27 +2,14 @@ require 'helper'
 
 class CacheTest < Test::Unit::TestCase
   def test_caching
-    unless App::Cache.store
-      App.logger.warn "No cache configed. Skipping cache tests"
-      return
-    end
-    
     App::Cache.clear
     
     done = 0
-    r = App.cached("six") do 
-      done += 1
-      2 * 3
-    end
 
-    assert_equal(6, r)
+    r = App.cached("six") { done += 1; 2 * 3 }
+    assert_equal([1, 6], [done, r])
     
-    r = App.cached("six") do 
-      done += 1
-      2 * 3
-    end
-
-    assert_equal(1, done)
-    assert_equal(6, r)
+    r = App.cached("six") { done += 1; 2 * 3 }
+    assert_equal([1, 6], [done, r])
   end
 end
